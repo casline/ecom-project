ca2a5ee93ea27d717f7f0b8908877f809671bcf3
casline <c_schafrath@yahoo.com>
2020-10-27 12:35:41 -0400

    built base structure for account components

---
+++ src/components/account/accountInformation.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AccountInformation extends Component {
+  render() {
+    return <div>Account Info</div>;
+  }
+}
+
+export default AccountInformation;
