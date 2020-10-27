ae8d80b22eb7f12ca054fbeb4f8db1c59855ced2
casline <c_schafrath@yahoo.com>
2020-10-27 15:25:35 -0400

    finished navbar feature

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -1,4 +1,8 @@
-import { SET_HEADER_LINKS, SET_NAVBAR_LINKS } from "../actions/types";
+import {
+  CHANGE_NAVBAR_ACTIVE,
+  SET_HEADER_LINKS,
+  SET_NAVBAR_LINKS,
+} from "../actions/types";
 
 const INITIAL_STATE = {
   headerLinks: [],
@@ -16,6 +20,18 @@ export default function (state = INITIAL_STATE, action) {
       return {
         ...state,
         navbarLinks: action.payload,
+      };
+    case CHANGE_NAVBAR_ACTIVE:
+      const navbarLinks = state.navbarLinks.map((link) => {
+        link.active = false;
+        if (link._id == action.payload) {
+          link.active = true;
+        }
+        return link;
+      });
+      return {
+        ...state,
+        navbarLinks,
       };
     default:
       return state;
