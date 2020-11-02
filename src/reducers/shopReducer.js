b137445d2694cc9a50884353b54db99c3879babe
casline <c_schafrath@yahoo.com>
2020-11-02 18:19:28 -0500

    filtered products feature complete

--- src/reducers/shopReducer.js
+++ src/reducers/shopReducer.js
@@ -1,3 +1,4 @@
+import { filterProductsWithCategoryId } from "../actions";
 import {
   SET_SHOP_CATEGORIES,
   SET_SHOP_PRODUCTS,
@@ -6,9 +7,8 @@ import {
 
 const INITIAL_STATE = {
   categories: [],
-  selectedCategoryId: 0,
-  productSelected: [],
   products: [],
+  filteredProducts: [],
 };
 
 export default function (state = INITIAL_STATE, action) {
@@ -25,10 +25,15 @@ export default function (state = INITIAL_STATE, action) {
         products: action.payload,
       };
     case FILTER_PRODUCTS_WITH_CATEGORY_ID:
-      console.log(action.payload);
+      var filteredProducts = [];
+      state.products.map((product) => {
+        if (product.belongsTo.includes(action.payload)) {
+          filteredProducts.push(product);
+        }
+      });
       return {
         ...state,
-        // selectedCategoryId,
+        filteredProducts,
       };
 
     default:
