f31af4d1479231339a3ac37c55f29bdbba34514a
casline <c_schafrath@yahoo.com>
2020-11-15 13:52:49 -0500

    worked on calculating up the costs from reviewForm.js and shopCart.js

--- src/components/order/review.js
+++ src/components/order/review.js
@@ -16,15 +16,30 @@ class Review extends Component {
     console.log("fields");
   };
   render() {
+    let subtotal = 0;
+    this.props.cartProducts.map((cartProduct) => {
+      subtotal += cartProduct.quantity * cartProduct.product.price;
+    });
     return (
       <div className="review">
         <PageTitle className="review__page-title" title="Order Review" />
-        <ReviewForm className="review__form" onSumbit={this.onSumbit} />
+        <ReviewForm
+          className="review__form"
+          onSumbit={this.onSumbit}
+          subtotal={subtotal}
+        />
       </div>
     );
   }
 }
 
-Review = connect(null, actions)(Review);
+function mapStateToProps(state) {
+  const { cartProducts } = state.user;
+  return {
+    cartProducts,
+  };
+}
+
+Review = connect(mapStateToProps, actions)(Review);
 
 export default Review;
